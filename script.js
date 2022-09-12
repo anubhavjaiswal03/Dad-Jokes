03b810de1936f207157a2db7c9e6c4e924ee7b72
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-09-12 22:39:54 +0530

    initial commit

--- script.js
+++ script.js
@@ -28,14 +28,41 @@ async function generateJoke() {
 	const res = await fetch('https://icanhazdadjoke.com', config);
 	const data = await res.json();
 
-	jokeEl.removeAttribute('close');
-	jokeEl.setAttribute('open', '');
 	jokeEl.innerText = data.joke;
+	expandElement(jokeEl, 'collapse');
 }
 
 jokeBtn.addEventListener('click', () => {
-	jokeEl.removeAttribute('open');
-	jokeEl.setAttribute('close', '');
-	document.querySelector('.container').classList.add('try');
+	expandElement(jokeEl, 'collapse');
 	generateJoke();
 });
+
+function expandElement(elem, collapseClass) {
+	// debugger;
+	elem.style.height = '';
+	elem.style.transition = 'none';
+
+	const startHeight = window.getComputedStyle(elem).height;
+
+	// Remove the collapse class, and force a layout calculation to get the final height
+	elem.classList.toggle(collapseClass);
+	const height = window.getComputedStyle(elem).height;
+
+	// Set the start height to begin the transition
+	elem.style.height = startHeight;
+
+	// wait until the next frame so that everything has time to update before starting the transition
+	requestAnimationFrame(() => {
+		elem.style.transition = '';
+
+		requestAnimationFrame(() => {
+			elem.style.height = height;
+		});
+	});
+
+	// Clear the saved height values after the transition
+	elem.addEventListener('transitionend', () => {
+		elem.style.height = '';
+		elem.removeEventListener('transitionend', arguments.callee);
+	});
+}
