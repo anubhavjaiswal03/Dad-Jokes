13ab87d695ab173bef5885af50af3cc005f3b60b
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-09-23 20:03:27 +0530

    minor change

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ async function generateJoke() {
 	setTimeout(() => {
 		jokeEl.removeAttribute('close');
 		jokeEl.setAttribute('open', '');
-	}, 400);
+	}, 350);
 }
 
 jokeBtn.addEventListener('click', () => {
