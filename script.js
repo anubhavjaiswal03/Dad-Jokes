fb7a8d179f674366902913c831cd14a2e8ade5d9
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-09-23 19:44:59 +0530

    change

--- script.js
+++ script.js
@@ -75,10 +75,10 @@ function expandElement(elem, collapseClass) {
 	});
 
 	// Clear the saved height values after the transition
-	// elem.addEventListener('transitionend', () => {
-	// 	elem.style.height = '';
-	// 	elem.removeEventListener('transitionend', arguments.callee);
-	// 	console.log('transition end');
-	// });
+	elem.addEventListener('transitionend', () => {
+		elem.style.height = '';
+		elem.removeEventListener('transitionend', arguments.callee);
+		console.log('transition end');
+	});
 	console.log('----------------------------------------------');
 }
